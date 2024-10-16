10e9dc101a60314abfdef8bd8a38b3e575d1958a
meustar <meustar829@gmail.com>
2024-10-16 13:28:12 +0900

    Step 한 개 추가

--- src/main/java/com/koreait/exam/springbatch_10/job/HelloWorld/HelloWorldConfig.java
+++ src/main/java/com/koreait/exam/springbatch_10/job/HelloWorld/HelloWorldConfig.java
@@ -22,20 +22,39 @@ public class HelloWorldConfig {
     public Job helloWorldJob() {
         return jobBuilderFactory.get("helloWorldJob")
                 .incrementer(new RunIdIncrementer())    // 강제로 매번 다른 ID를 실행할 때 파라미터로 부여
-                .start(helloWorldStep1()).build();
+                .start(helloWorldStep1())
+                .next(helloWorldStep2())
+                .build();
     }
     @Bean
     @JobScope
     public Step helloWorldStep1() {
-        return stepBuilderFactory.get("helloWorldStep1").tasklet(helloWorldTasklet()).build();
+        return stepBuilderFactory.get("helloWorldStep1")
+                .tasklet(helloWorldSept1Tasklet())
+                .build();
     }
     @Bean
     @StepScope
-    public Tasklet helloWorldTasklet() {
+    public Tasklet helloWorldSept1Tasklet() {
         return (stepContribution, chunkContext) -> {
-            System.out.println("helloWorld!!!");
+            System.out.println("헬로월드11111111");
             return RepeatStatus.FINISHED;
         };
     }
-    //Job :여러가지의  Step들로 구성
+
+    @Bean
+    @JobScope
+    public Step helloWorldStep2() {
+        return stepBuilderFactory.get("helloWorldStep2")
+                .tasklet(helloWorldStep2Tasklet())
+                .build();
+    }
+    @Bean
+    @StepScope
+    public Tasklet helloWorldStep2Tasklet() {
+        return (stepContribution, chunkContext) -> {
+            System.out.println("헬로월드22222222");
+            return RepeatStatus.FINISHED;
+        };
+    }
 }
