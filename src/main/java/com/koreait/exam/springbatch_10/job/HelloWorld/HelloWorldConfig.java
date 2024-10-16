577cff6f5d1dff553a88e7ee5ad9878d9cff13ca
meustar <meustar829@gmail.com>
2024-10-16 13:19:54 +0900

    Bean, Job, Step Scope

--- src/main/java/com/koreait/exam/springbatch_10/job/HelloWorld/HelloWorldConfig.java
+++ src/main/java/com/koreait/exam/springbatch_10/job/HelloWorld/HelloWorldConfig.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -23,10 +25,12 @@ public class HelloWorldConfig {
                 .start(helloWorldStep1()).build();
     }
     @Bean
+    @JobScope
     public Step helloWorldStep1() {
         return stepBuilderFactory.get("helloWorldStep1").tasklet(helloWorldTasklet()).build();
     }
     @Bean
+    @StepScope
     public Tasklet helloWorldTasklet() {
         return (stepContribution, chunkContext) -> {
             System.out.println("helloWorld!!!");
